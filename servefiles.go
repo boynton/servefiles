d39f0beb3396d3d5536d239a5b53a87d34d60c4b
Lee R. Boynton <lee@boynton.com>
2017-01-22 15:16:39 -0800

    Show more useful URL information, cleaned up some stuff.

--- servefiles.go
+++ servefiles.go
@@ -3,28 +3,53 @@ package main
 import (
 	"flag"
 	"fmt"
-	"os"
+	"net"
 	"net/http"
+	"os"
+	"strings"
 )
 
-func main() {
-	flag.Usage = func() {
-		fmt.Println("Usage: servefiles [-e [host]:port] directory")
+func defaultEndpoint(port int) (string, error) {
+	ifaces, err := net.Interfaces()
+	if err != nil {
+		return "", err
 	}
-	endpoint := flag.String("e", ":8080", "endpoint ([host]:port")
-	flag.Parse()
-	args := flag.Args()
-	if len(args) == 1 && len(*endpoint) > 0 {
-		s := *endpoint
-		if s[0] == ':' {
-			s = "0.0.0.0" + s
+	for _, i := range ifaces {
+		addrs, err := i.Addrs()
+		if err != nil {
+			return "", err
 		}
-		fmt.Printf("Serving files from %s at http://%s/\n", args[0], s)
-		err := http.ListenAndServe(*endpoint, http.FileServer(http.Dir(args[0])))
+		for _, a := range addrs {
+			addr := fmt.Sprint(a)
+			if strings.HasPrefix(addr, "192.") || strings.HasPrefix(addr, "10.") || strings.HasPrefix(addr, "172.") {
+				s := strings.Split(addr, "/")
+				return fmt.Sprintf("%s:%d", s[0], port), nil
+			}
+		}
+	}
+	return fmt.Sprintf("127.0.0.1:%d", port), nil
+}
+
+func main() {
+	pPort := flag.Int("p", 8080, "port to listen on")
+	flag.Parse()
+	endpoint, err := defaultEndpoint(*pPort)
+	if err != nil {
+		fmt.Printf("*** %v\n", err)
+		os.Exit(1)
+	}
+	flag.Usage = func() {
+		fmt.Println("Usage: servefiles [-p port] directory")
+	}
+	args := flag.Args()
+	if len(args) > 0 {
+		fmt.Printf("Serving files from %s at http://%s/\n", args[0], endpoint)
+		err = http.ListenAndServe(endpoint, http.FileServer(http.Dir(args[0])))
 		if err != nil {
 			fmt.Printf("*** %v\n", err)
 			os.Exit(1)
 		}
+	} else {
+		flag.Usage()
 	}
-	flag.Usage()
 }
